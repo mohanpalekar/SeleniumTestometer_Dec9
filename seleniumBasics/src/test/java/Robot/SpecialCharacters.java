90538494acce2a4cc9c664ff35b411ce19e21ef5
palek <palek@192.168.1.37>
2025-01-08 09:03:33 +0530

    Add Robot class, upload image

---
+++ seleniumBasics/src/test/java/Robot/SpecialCharacters.java
@@ -0,0 +1,12 @@
+package Robot;
+
+public class SpecialCharacters {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		
+		System.out.println("Name" +"\n" +"Income");
+	}
+
+}
