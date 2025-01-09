9315d55da22c60b600af9c494c91989138810d41
palek <palek@192.168.1.37>
2025-01-09 09:01:54 +0530

    Add DatePiker, WebTables

---
+++ seleniumBasics/src/test/java/datePicker/SelectDate.java
@@ -0,0 +1,124 @@
+package datePicker;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SelectDate {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+		driver.manage().window().maximize();
+
+		String url = "https://www.makemytrip.com/";
+
+		driver.get(url);
+
+		WebElement signUpPopUp = wait.until(ExpectedConditions.elementToBeClickable(By.className("commonModal__close")));
+
+		jse.executeScript("arguments[0].setAttribute('style', 'border: 4px solid green;');", signUpPopUp);
+
+		signUpPopUp.click();
+
+		WebElement departure = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//label[@for='departure']")));
+
+		jse.executeScript("arguments[0].setAttribute('style', 'border: 4px solid green;');", departure);
+
+		departure.click();
+
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Map<String, Integer> month = new HashMap<>();
+		
+		month.put("Jan", 1);
+		month.put("Feb", 2);
+		month.put("Mar", 3);
+		month.put("Apr", 4);
+		month.put("May", 5);
+		month.put("Jun", 6);
+		month.put("Jul", 7);
+		month.put("Aug", 8);
+		month.put("Sep", 9);
+		month.put("Oct", 10);
+		month.put("Nov", 11);
+		month.put("Dec", 12);
+		
+		List<WebElement> months = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@class='DayPicker-Month']/div[@role='heading']/div")));
+
+		for(WebElement m : months) {
+			System.out.println(m.getText());
+		}
+		
+		WebElement nextMonth = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[@aria-label='Next Month']")));
+
+		jse.executeScript("arguments[0].setAttribute('style', 'border: 4px solid green;');", nextMonth);
+
+		nextMonth.click();
+		
+		
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		WebElement prevMonth = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[@aria-label='Previous Month']")));
+
+		jse.executeScript("arguments[0].setAttribute('style', 'border: 4px solid green;');", prevMonth);
+
+		prevMonth.click();
+		
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		WebElement dateToSelect = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@aria-label='Wed Feb 26 2025']")));
+		
+		jse.executeScript("arguments[0].setAttribute('style', 'border: 4px solid green;');", dateToSelect);
+
+		dateToSelect.click();
+		
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.close();
+
+
+	}
+
+}
